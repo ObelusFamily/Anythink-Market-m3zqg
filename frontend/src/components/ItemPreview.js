6f02ff0336dbe77de3f5a544112ab840b35ff2dc
LCox3236 <83649281+LCox3236@users.noreply.github.com>
2022-09-30 11:53:52 +0100

    Update ItemPreview.js

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -36,7 +36,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image.length > 0 ? item.image : "../placeholder.png"}
+        src={item.image !== undefined ? "../placeholder.png" : item.image }
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
